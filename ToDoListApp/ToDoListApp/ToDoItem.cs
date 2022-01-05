f19826025ab6719a69d8991ef86e416d5d6a7e4c
Fernando Delgado <fernando.seattle.2016@gmail.com>
2022-01-05 16:49:46 -0600

    sample project, initial commit

---
+++ ToDoListApp/ToDoListApp/ToDoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ToDoListApp
+{
+	public class ToDoItem
+	{
+		public string TodoText { get; set; }
+		public bool Complete { get; set; }
+
+		public ToDoItem(string TodoText, bool Complete)
+		{
+			this.TodoText = TodoText;
+			this.Complete = Complete;
+		}
+	}
+}
+
---
+++ ToDoListApp/ToDoListApp/TodoListViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+using System.Collections.ObjectModel;
+using Xamarin.Forms;
+namespace ToDoListApp
+{
+	public class TodoListViewModel
+	{
+		public ObservableCollection<ToDoItem> TodoItems { get; set; }
+
+		public TodoListViewModel()
+		{
+			TodoItems = new ObservableCollection<ToDoItem>();
+
+			TodoItems.Add(new ToDoItem("todo 1", false));
+			TodoItems.Add(new ToDoItem("todo 2", false));
+			TodoItems.Add(new ToDoItem("todo 3", false));
+
+		}
+
+		public ICommand AddTodoCommand => new Command(AddTodoItem);
+		public string NewTodoInputValue { get; set; }
+		void AddTodoItem()
+		{
+			TodoItems.Add(new ToDoItem(NewTodoInputValue, false));
+		}
+	}
+}
+
